82592a4a0125d443880320b33f0849cf2588b089
krishna <krishna.patel@meetanshi.com>
2025-03-24 15:58:22 +0530

    changes forr style

--- resources/views/flipBook.blade.php
+++ resources/views/flipBook.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pdf Preview</title>
  
-    <link rel="stylesheet" href="{{ asset('public/FlipPdfJs/style.css')}}"></link>
+    <link rel="stylesheet" href="{{ asset('FlipPdfJs/style.css')}}"></link>
 </head>
 
 <body>
